510c23a7cc619e6d7050b906d584bb6a6b538cba
Sam Pepose <sampepose@gmail.com>
2016-02-07 18:23:05 -0600

    Added data gathering for accuracy, auc, and top k features

--- README.md
+++ README.md
@@ -4,9 +4,11 @@ Classify spambase dataset: https://archive.ics.uci.edu/ml/datasets/Spambase
 Accuracy is based on a 70/30 training set split averaged for 50 runs.
 ## sklearn
 #### Naive Bayes
-Gaussian: 80.552%
-Multinomial (alpha=1.0): 86.728%
-Bernoulli (alpha=1.0, binarize=0.31): 89.163%
+| Method | Accuracy Avg | Accuracy Std | AUC Avg | AUC Std | Top 5 Features |
+| --- | --- | --- | --- | --- | --- |
+| Gaussian | 0.808000 | 0.00781 | 0.84982 | 0.00714 | 'meeting' >>> 'data', '85', 'labs', 'conference' |
+| Multinomial | 0.87230 | 0.00714 | 0.95302 | 0.00390 | 'meeting', 'data', '857', 'labs', 'conference' |
+| Bernoulli | 0.89164 | 0.00528 | 0.95017 | 0.00403 | 'labs' = 'meeting', 'data', '650' = '85' |
 
 #### Decision Trees
 // TODO
--- naiveBayes/sklearn_test.py
+++ naiveBayes/sklearn_test.py
@@ -3,12 +3,24 @@
 from load_data import load_data
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.grid_search import GridSearchCV
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+import numpy as np
+import operator
+
+word_labels = ['address', 'all', '3d', 'our', 'over', 'remove', 'internet',
+               'order', 'mail', 'receive', 'will', 'people', 'report', 'addresses',
+               'free', 'business', 'email', 'you', 'credit', 'your', 'font', '000',
+               'money', 'hp', 'hpl', 'george', '650', 'lab', 'labs', 'telnet', '857',
+               'data', '415', '85', 'technology', '1999', 'parts', 'pm', 'direct',
+               'cs', 'meeting', 'original', 'project', 're', 'edu', 'table', 'conference']
 
 class METHOD:
     gaussian, multinomial, bernoulli = range(3)
     
 method = METHOD.bernoulli
 iterations = 50
+k = 5
     
 def find_hyperparams_bernoulli(clf, X, y):
     # Set the parameters by cross-validation
@@ -16,9 +28,28 @@ def find_hyperparams_bernoulli(clf, X, y):
     grid = GridSearchCV(clf, param_grid)
     grid.fit(X, y)
     print('done fitting')
-    return grid.best_estimator_
+    return grid.best_estimator_    
+    
+def show_auc(y_true, y_score):
+    fpr, tpr, _ = roc_curve(y_true, y_score)
+    roc_auc = auc(fpr, tpr)
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+    
+def top_k_features(k, weights):
+    return sorted(zip(word_labels, weights), key=operator.itemgetter(1))[:k]
 
-avg = 0
+scores = []
+roc_auc = []
+weights = []
 
 for i in range(iterations):
     X_train, X_test, y_train, y_test = load_data(Train=True)
@@ -44,6 +75,19 @@ for i in range(iterations):
     #    print(find_hyperparams_bernoulli(clf, X[:, 0:48], y))
         
     clf.fit(X_train, y_train)
-    avg += clf.score(X_test, y_test)
+    scores.append(clf.score(X_test, y_test))
     
-print(avg / iterations)
+    fpr, tpr, _ = roc_curve(y_test, clf.predict_proba(X_test)[:, 1])
+    roc_auc.append(auc(fpr, tpr))
+    
+    if method == METHOD.gaussian:
+        weights = clf.theta_[1, :]
+    else:
+        weights = clf.feature_log_prob_[1, :]
+
+show_auc(y_test, clf.predict_proba(X_test)[:, 1])
+    
+print('Accuracy. Avg: %0.5f, Std: %0.5f' % (np.mean(scores), np.std(scores)))
+print('AUC. Avg: %0.5f, Std: %0.5f' % (np.mean(roc_auc), np.std(roc_auc)))
+print('Top %d features:' % k)
+print(top_k_features(k, weights))
